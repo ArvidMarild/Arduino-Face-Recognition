b9d7add6b5aa5d61d8aa886a299e0e3cbc199a90
ArvidMarild <152278740+ArvidMarild@users.noreply.github.com>
2024-05-23 17:50:18 +0200

    Update arduino_communication.ino

--- arduino_communication/arduino_communication.ino
+++ arduino_communication/arduino_communication.ino
@@ -1,25 +1,28 @@
-#define led 7
-int data;
+#define led 7 // Define pin 7 as the led pin
+int data; // Variable to store incoming serial data
 
 void setup() {
-  pinMode(led, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
-  digitalWrite(led, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(led, OUTPUT); // Set pin 7 as an output
+  pinMode(LED_BUILTIN, OUTPUT);   // Set the built-in LED as an output
+  Serial.begin(9600);             // Initialize serial communication at 9600 baud
+  digitalWrite(led, LOW);         // Turn off the LED at pin 7
+  digitalWrite(LED_BUILTIN, LOW); // Turn off the built-in LED
 }
 
 void loop() {
-  while(Serial.available()) {
-    data = Serial.read();
+  // Check if data is available to read from the serial port
+  while (Serial.available()) {
+    data = Serial.read();  // Read the incoming data
 
-    if (data == '1'){
-      digitalWrite(led, HIGH);
-      digitalWrite(LED_BUILTIN, HIGH);
+    // If the data is '1', turn on both LEDs
+    if (data == '1') {
+      digitalWrite(led, HIGH);         // Turn on the LED at pin 7
+      digitalWrite(LED_BUILTIN, HIGH); // Turn on the built-in LED
     }
+    // If the data is '0', turn off both LEDs
     else if (data == '0') {
-      digitalWrite(led, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(led, LOW);          // Turn off the LED at pin 7
+      digitalWrite(LED_BUILTIN, LOW);  // Turn off the built-in LED
     }
   }
 }
